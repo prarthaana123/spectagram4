5348bbdadadc37bebb8c8c8f39374516d95244e5
prarthaana123 <78807649+prarthaana123@users.noreply.github.com>
2021-09-26 18:25:44 +0530

    Add files via upload

---
+++ navigation/TabNavigator.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Feed from "../screens/Feed";
+import CreatePost from  "../screens/CreatePost";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { RFValue } from "react-native-responsive-fontsize";
+import {StyleSheet,} from "react-native"
+
+
+const Tab = createBottomTabNavigator();
+
+const BottomTabNavigator=()=>{
+    return(
+        <Tab.Navigator 
+        labeled={false}
+        barStyle={styles.bottomTabStyle}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === "Feed") {
+              iconName = focused ? "home" : "home-outline";
+            } else if (route.name === "Create Post") {
+              iconName = focused ? "add-circle" : "add-circle-outline";
+            }
+            return (
+              <Ionicons
+                name={iconName}
+                size={RFValue(25)}
+                color={color}
+                style={styles.icons}
+              />
+            );
+          }
+        })}
+        activeColor={"#ee8249"}
+        inactiveColor={"gray"}
+      >
+        <Tab.Screen name="Feed" component={Feed} />
+        <Tab.Screen name="Create Post" component={CreatePost} />
+        </Tab.Navigator>
+     
+    );
+  }
+
+  export default BottomTabNavigator;
+  const styles = StyleSheet.create({
+    bottomTabStyle: {
+      backgroundColor: "#2f345d",
+      height: "8%",
+      borderTopLeftRadius: 30,
+      borderTopRightRadius: 30,
+      overflow: "hidden",
+      position: "absolute"
+    },
+    icons: {
+      width: RFValue(30),
+      height: RFValue(30)
+    }
+  });
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
